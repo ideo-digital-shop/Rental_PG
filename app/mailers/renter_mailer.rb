37883b9fa6b846a7b1ed3008697dab2175b326c8
Kevin Ho <kho@ideo.com>
2012-10-01 16:16:36 -0700

    Adding email

---
+++ app/mailers/renter_mailer.rb
@@ -0,0 +1,7 @@
+class RenterMailer < ActionMailer::Base
+  default from: "from@example.com"
+  def reminderEmail(rental)
+  	@rental = rental
+  	@url = 'rentsys.herokuapp.com'
+  	mail(:to => rental.email, :subject => "Welcome to My Awesome Site")
+end
