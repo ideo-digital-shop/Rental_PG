df5c0662b750417c7faf03bc81b4afd893d9c361
Kevin Ho <kho@ideo.com>
2012-10-02 10:05:15 -0700

    Added config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Main::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.delivery_method = :smtp
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
