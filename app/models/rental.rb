60b6c642773a9ed021e38f80ee2b84f27c4d321b
Kevin Ho <kho@ideo.com>
2012-10-03 10:05:49 -0700

    Updated emailing system

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,7 +2,7 @@ class Rental < ActiveRecord::Base
   validates :email, :uniqueness => { :case_sensitive => false }
   attr_accessible :device_id, :email, :end, :renter
   belongs_to :device
-  def emailRenters
+  def self.emailRenters
     for renter in self.where("end < ?", Date.current())
       RenterMailer.reminderEmail(renter).deliver
     end
