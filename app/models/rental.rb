17fc4487fc3d67c8782c260c13d53d0409e97604
Kevin Ho <kho@ideo.com>
2012-10-04 10:39:06 -0700

    Added uniqueness check for id

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,5 +1,6 @@
 class Rental < ActiveRecord::Base
   validates :email, :uniqueness => { :case_sensitive => false }
+  validates :device_id, :uniqueness => true
   attr_accessible :device_id, :email, :end, :renter
   belongs_to :device
   def self.emailRenters
