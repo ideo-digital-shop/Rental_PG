b70c0f8ba368458906c282f489a069f01977d668
Kevin Ho <kho@ideo.com>
2012-10-04 10:45:47 -0700

    Fixing up validation

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,6 @@
 class Device < ActiveRecord::Base
   validates :device_id, :uniqueness => true
   attr_accessible :device_id, :device_type
+  validates_presence_of :device_id, :device_type
   has_one :rental
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,8 @@
 class Rental < ActiveRecord::Base
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :device_id, :uniqueness => true
+  validates_associated :device
+  validates_presence_of :device_id, :email, :end, :renter
   attr_accessible :device_id, :email, :end, :renter
   belongs_to :device
   def self.emailRenters
