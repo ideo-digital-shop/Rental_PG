d8f428f5b8e5eeb79dbc7d1c804a0fad6229fbd9
Kevin Ho <kho@ideo.com>
2012-10-04 12:58:07 -0700

    Making checks that associated devices exists

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,7 +2,7 @@ class Rental < ActiveRecord::Base
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :device_id, :uniqueness => true
   validates_associated :device
-  validates_presence_of :device_id, :email, :end, :renter
+  validates_presence_of :device_id, :email, :end, :renter, :device
   attr_accessible :device_id, :email, :end, :renter
   belongs_to :device
   def self.emailRenters
