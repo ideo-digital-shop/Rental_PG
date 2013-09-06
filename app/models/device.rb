3161b6dc7f0bf7429f5d103b44b65163905dbf68
Kevin Ho <keho98@gmail.com>
2013-09-05 17:16:43 -0700

    Added new field accessors

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,6 @@
 class Device < ActiveRecord::Base
   validates :device_id, :uniqueness => true
-  attr_accessible :device_id, :device_type
+  attr_accessible :device_id, :device_type, :additional_information
   validates_presence_of :device_id, :device_type
   has_one :rental
 end
